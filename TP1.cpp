afc41ad58028b6c8dafbbebea983b68e4696b3d8
luckavaz95 <luckavaz95@gmail.com>
2015-04-14 15:26:55 -0300

    TP1
    Tipos de Datos

---
+++ TP1.cpp
@@ -0,0 +1,41 @@
+/* Trabajo Práctico 1 Tipos de Datos
+* Lucas Vázquez K1091
+* 14/04/2015
+*/
+
+#include <iostream>
+
+using namespace std;
+int main(){
+	int A, B, C;
+	bool D;
+	char E;
+	double F;
+	string G, H;
+	cout<<"Ingrese un caracter para identificar el resultado \n";
+	cin>>E;
+	cout<<"Ingrese un numero entero A \n";
+	cin>>A;
+	cout<<"Ingrese un numero entero B \n";
+	cin>>B;
+	C=A+B;
+	cout<<E;
+	cout<<"=";
+	cout<<C;
+	cout<<endl;
+	cout<<"A+B/2=";
+	F=C/2.0;
+	cout<<F;
+	cout<<endl;
+	D=C%2;
+	if (D==false){
+	cout<<E;
+	cout<<" un numero par";}
+	else{
+	cout<<E;
+	cout<<" es un numero impar";};
+	cout<<endl;
+	G="Fin del ";
+	H="Programa";
+	cout<<G+H;
+}
